6665881aa51cd84f87c60c9a653a421babdb122e
Skotarenko Denys <skotarenkodenis@gmail.com>
2021-06-05 15:26:59 +0300

    Added basic structure

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP - 1</title>
+    <style>
+        li{
+            list-style: none;
+            margin-left: 15px;
+        }
+        a{ text-decoration: none; }
+        a:hover{ color: red; }
+        .menu_list{ display: flex; }
+    </style>
+</head>
+<body>
+    <header>
+        <ul class="menu_list">
+            <li><a href="/hw2/">Home work 1</a></li>
+            <li><a href="/hw2/">Home work 2</a></li>
+            <li><a href="/hw2/">Home work 3</a></li>
+            <li><a href="/hw2/">Home work 4</a></li>
+            <li><a href="/hw2/">Home work 5</a></li>
+            <li><a href="/hw2/">Home work 6</a></li>
+            <li><a href="/hw2/">Home work 7</a></li>
+            <li><a href="/hw2/">Home work 8</a></li>
+        </ul>
+    </header>
+</body>
+</html>
