f2fea597309e146af2baf789323f0098b85757ca
Skotarenko Denys <skotarenkodenis@gmail.com>
2021-06-06 12:44:16 +0300

    executed successfully hot_fix_hw3

--- hw3/index.php
+++ hw3/index.php
@@ -18,23 +18,26 @@
 <?php
 
 if(isset($_GET['fNumber']) && isset($_GET['sNumber'])){
-    $fNumber = $_GET['fNumber'];
-    $sNumber = $_GET['sNumber'];
-    $sign = $_GET['sign'];
+    if(is_numeric($_GET['fNumber']) && is_numeric($_GET['sNumber']) && 0 != $_GET['sNumber']){
 
-    switch ($sign){
-        case '+':
-            $result =  $fNumber + $sNumber;
-            break;
-        case '-':
-            $result =  $fNumber - $sNumber;
-            break;
-        case '*':
-            $result =  $fNumber * $sNumber;
-            break;
-        case '/':
-            $result =  $fNumber / $sNumber;
-            break;
+        $fNumber = $_GET['fNumber'];
+        $sNumber = $_GET['sNumber'];
+        $sign = $_GET['sign'];
+
+        switch ($sign){
+            case '+':
+                $result =  $fNumber + $sNumber;
+                break;
+            case '-':
+                $result =  $fNumber - $sNumber;
+                break;
+            case '*':
+                $result =  $fNumber * $sNumber;
+                break;
+            case '/':
+                $result =  $fNumber / $sNumber;
+                break;
+        }
     }
 }
 
